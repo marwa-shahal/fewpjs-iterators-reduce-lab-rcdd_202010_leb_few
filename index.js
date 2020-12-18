888e957d128d87afe4f6cb6aaea0ede1e2403ed5
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-18 18:22:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // function superbowlWin() 
 function reducer () { function batteries(){
   let totalBatteries;
-  batteryBatches.find(s => )
   batteryBatches.reduce((sum,m) => sum+m);
 }}
