94d340d78ffd55f4341750ecd4fd6b011f0aa855
Marwa Sh <marwa.al.shahal@gmail.com>
2020-12-18 18:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 // function superbowlWin() 
-function reducer () { function batteries(){
-  let totalBatteries=0;
-  batteryBatches.reduce((sum,m) => sum+m);
+function reducer () { function batteries(totalBatteries){
+  //let totalBatteries=0;
+  //batteryBatches.reduce((sum,m) => sum+m);
+  totalBatteries.reduce((sum,m) => sum+m);
 }}
